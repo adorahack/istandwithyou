e2c2dcafe530ebe367fffb7b4c0bb0896f1384f5
nennenwodo <nennenwodo@gmail.com>
2019-02-10 01:39:04 +0100

    Sorted results accoring to number of votes, Added percentage to progress bar

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -7,8 +7,11 @@
                 <p>Total Votes: <strong>{{votes.length}}</strong></p>
                 <br />
                 <div v-for="candidate in candidates" :key="candidate.id">
-                    <p>{{ candidate.name }} ({{ candidate.party }})</p>
-                    <b-progress :value="candidate.votes.length" :max="votes.length" show-progress variant="success"></b-progress>
+                    <p>{{ candidate.name }} ({{ candidate.party }}) - {{candidate.votes.length}} votes</p>
+                    <b-progress :max="votes.length" variant="success" show-progress>
+                      <b-progress-bar :value="candidate.votes.length" :label="`~${ Math.round(candidate.votes.length / votes.length * 100)}%`">
+                      </b-progress-bar>
+                    </b-progress>
                     <br />
                 </div>
             </div>
@@ -42,13 +45,13 @@ export default {
   methods: {
     getAllCandidates: function(){
       candidates().then((results) => {
-        console.log(results)
-        this.candidates = results
+        // console.log(results)
+        this.candidates = results.sort((a,b) => b.votes.length - a.votes.length)
       })
     },
     getAllVotes: function(){
       votes().then((results) => {
-        console.log(results)
+        // console.log(results)
         this.votes = results
       })
     }
